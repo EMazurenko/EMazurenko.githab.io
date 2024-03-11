726a806682faaf3437a7bd8c52a2d17d30563904
Mazurenko-EV <MazurenkoEV@intech.rshb.ru>
2024-03-11 13:38:25 +0300

    ДЗ: TypeScript. Начало

---
+++ src/homeworks/ts1/2_repair.test.ts
@@ -0,0 +1,31 @@
+import {Data, getDataAmount} from './2_repair'
+
+describe('all', () => {
+    it('getDataAmount Money', () => {
+      const arg: Data = {
+        type: 'Money',
+        value: {
+            amount: 100,
+            currency: '810'
+        }
+      }
+  
+      const expected = 100
+  
+      expect(getDataAmount(arg)).toEqual(expected)
+    })
+  });
+
+
+  describe('all', () => {
+    it('getDataAmount Percent', () => {
+      const arg: Data = {
+        type: 'Percent',
+        value: {
+            percent: 0.25
+        }
+      }
+  
+      expect(() => getDataAmount(arg)).toThrow(`unknown type: ${arg.type}`)
+    })
+  });
