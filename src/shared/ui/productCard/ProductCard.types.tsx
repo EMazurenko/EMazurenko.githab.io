0a52556c863100f4f9491ec786a4435861a82e9d
Mazurenko-EV <MazurenkoEV@intech.rshb.ru>
2024-04-07 19:17:31 +0300

    ДЗ: Осваиваем Typescript - Мелкая правка

--- src/shared/ui/productCard/ProductCard.types.tsx
+++ src/shared/ui/productCard/ProductCard.types.tsx
@@ -1,15 +1,14 @@
 export type ProductCardContainerProps = {
-  type: 'Short' | 'Full' | 'InCart';
+    type: 'Short' | 'Full' | 'InCart',
 } & Partial<ProductCardProps>;
 
 export type ProductCardProps = {
-  category?: string;
-  title: string;
-  description: string;
-  price: number;
-  initCountItems: number;
-  photo_url: string | string[];
-  handlerCountItem: (newCountItem: number) => void;
+    category?: string,
+    title: string,
+    description: string,
+    price: number,
+    initCountItems: number,
+    photo_url: string | string[],
+    handlerCountItem: (newCountItem: number) => void,
 };
 
-export type InCartProductCardProps = Omit<ProductCardProps, 'price' | 'description'> & { sum: number };
