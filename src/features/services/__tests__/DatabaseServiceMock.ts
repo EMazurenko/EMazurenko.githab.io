ecac87fb7d226eb26a4570f42010688a8bccd083
Mazurenko-EV <MazurenkoEV@intech.rshb.ru>
2024-06-03 09:56:22 +0300

    ДЗ: Реализация AccountService через подход Test Driven Development подход - Исправление ошибки наличия файла без теста в директории __tests__

--- src/features/services/__tests__/DatabaseServiceMock.ts
+++ src/features/services/__tests__/DatabaseServiceMock.ts
@@ -1,8 +1,12 @@
 import DatabaseService from '../DatabaseService';
 import DataSource from '../DataSource';
-import { ACCOUNT_TYPES, CATEGORIES } from './consts';
+import { CAR_CATEGORY, FOOD_CATEGORY, TOY_CATEGORY } from './consts';
+import { AccountType } from 'src/entities/profile/model/types';
 
 jest.mock('../DataSource');
+
+const ACCOUNT_TYPES = [AccountType.STANDARD, AccountType.PREMIUM, AccountType.GOLD, AccountType.FREE];
+const CATEGORIES = [CAR_CATEGORY, TOY_CATEGORY, FOOD_CATEGORY];
 
 describe('DatabaseServiceMock', () => {
   let databaseService: DatabaseService;
--- src/features/services/__tests__/consts.ts
+++ src/features/services/__tests__/consts.ts
@@ -1,9 +1,11 @@
-import { AccountType } from 'src/entities/profile/model/types';
 import { Category } from 'src/entities/category/model/types';
 
 export const CAR_CATEGORY: Category = { id: '1', name: 'Car' };
 export const TOY_CATEGORY: Category = { id: '2', name: 'Toy' };
 export const FOOD_CATEGORY: Category = { id: '3', name: 'Food' };
 
-export const CATEGORIES = [CAR_CATEGORY, TOY_CATEGORY, FOOD_CATEGORY];
-export const ACCOUNT_TYPES = [AccountType.STANDARD, AccountType.PREMIUM, AccountType.GOLD, AccountType.FREE];
+describe('Const', () => {
+  it('Fix error "no test file in __test__"', () => {
+    expect(true).toBeTruthy();
+  });
+});
