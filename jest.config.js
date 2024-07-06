55134bc412c93210d04d09f077b049511550705c
Mazurenko-EV <MazurenkoEV@intech.rshb.ru>
2024-07-06 08:50:17 +0300

    ДЗ: Роутинг - Исправление ошибки тестов

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/jestSetupFile.js'],
   moduleNameMapper: {
-    '^src(.*)$': '<rootDir>/src$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
+    '^src(.*)$': '<rootDir>/src$1',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
   transformIgnorePatterns: ['/node_modules/(?!react-file-drop)'],
