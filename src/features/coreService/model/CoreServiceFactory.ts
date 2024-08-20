138043e0c594b59d902a114c05a9dc09e0d489cd
Mazurenko-EV <MazurenkoEV@intech.rshb.ru>
2024-08-20 15:02:49 +0300

    Проектная работа - Настройка режима работы сервисов

--- src/features/coreService/model/CoreServiceFactory.ts
+++ src/features/coreService/model/CoreServiceFactory.ts
@@ -4,6 +4,8 @@ import { CoreServiceRest } from 'src/features/coreService/model/CoreServiceRest'
 
 const COMMAND_ID = '202402_MAZURENKO_EV';
 
+console.log('Service Mode: ', process.env.REACT_APP_USE_SERVICES_MODE);
+
 export const coreService: CoreService =
   process.env.REACT_APP_USE_SERVICES_MODE === 'rest'
     ? new CoreServiceRest(process.env.REACT_APP_SERVER_REST_URL, COMMAND_ID)
